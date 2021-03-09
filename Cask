07ce175930be90d5dd674745801dbe15b06eccf8
Detlev V <detvdael.services@protonmail.com>
2021-03-09 16:47:39 +0100

    Initial version of the VRT NU package

---
+++ Cask
@@ -0,0 +1,15 @@
+(source gnu)
+(source melpa)
+
+(package "vrtnu" "0.0.1" "VRTNU news viewing for Emacs.")
+
+(package-file "vrtnu.el")
+
+(depends-on "mpv" :git "https://github.com/detvdl/mpv.el" :ref "70d9084ade699e85cca46212ac1dcaf9b9c5cd6a" :branch "master")
+(depends-on "dash")
+
+(development
+ (depends-on "f")
+ (depends-on "ecukes")
+ (depends-on "ert-runner")
+ (depends-on "el-mock"))
---
+++ vrtnu.el
@@ -0,0 +1,73 @@
+;;; vrtnu.el --- Emacs support for the Belgian VRT NU news format.
+
+;; Copyright (C) 2010-2020 detvdl
+
+;; Author: Detlev Vandaele <detvdl@pm.me>
+;; Maintainer: Detlev Vandaele <detvdl@pm.me>
+;; Created: 9 March 2021
+
+;; Version: 0.0.1
+;; Package-Version: 0.0.1
+
+;; Package-requires (dash mpv (emacs "27"))
+
+;;; Commentary:
+;;; Code:
+
+(require 'dash)
+(require 'mpv)
+
+(defgroup vrtnu nil
+  "VRT NU for emacs.")
+
+(defcustom vrtnu-config-file (locate-user-emacs-file ".vrtnu.eld")
+  "The position of the file that holds your user configuration for
+accessing the VRT NU website."
+  :type 'file
+  :group 'vrtnu)
+
+(defun vrtnu--completion-table (items)
+  "Generate completion table from ITEMS collection that maintains order in function using `metadata'."
+  (lambda (string pred action)
+    (if (eq action 'metadata)
+        '(metadata (display-sort-function . identity)
+                   (cycle-sort-function . identity))
+      (complete-with-action action items string pred))))
+
+(defun vrtnu--prompt-date (&optional days)
+  "Prompt user for a date.
+Optional DAYS argument can be passed to restrict amount of days shown.
+Default is 31."
+  (let ((days '()))
+    (dotimes (i (or days 31))
+      (setf days (cons (time-subtract (current-time) (days-to-time i)) days)))
+    (let* ((day-seq (nreverse (--annotate (format-time-string "%A - %d/%m/%Y" it) days)))
+           (ivy-sort-functions-alist nil)
+           (input (completing-read "Date: " (vrtnu--completion-table day-seq) nil t nil)))
+      (cdr (assoc input day-seq)))))
+
+(defun vrtnu--select-date-time ()
+  "Select date-time for VRT news selection."
+  (-let* ((hours-alist '(("13u" . 13) ("update" . 17) ("19u" . 19) ("laat" . 23)))
+          (date (decode-time (vrtnu--prompt-date 7)))
+          (available-hours (--select (time-less-p (encode-time (-replace-at 2 (cdr it) date)) nil) hours-alist))
+          (ivy-sort-functions-alist nil)
+          (time (completing-read "Time: " (vrtnu--completion-table available-hours) nil t nil)))
+    `(:date ,(encode-time date) :time ,time)))
+
+;;;###autoload
+(defun vrt-news ()
+  "Play selected news from vrt.be/vrtnu."
+  (interactive)
+  (-let* (((&alist 'username user 'password pass)
+           (with-current-buffer (find-file-noselect vrtnu-config-file)
+             (goto-char (point-min))
+             (read (buffer-string))))
+          ((&plist :date date :time time) (vrtnu--select-date-time))
+          (date-fmt (format-time-string "%Y%m%d" date))
+          (news-url (format "https://www.vrt.be/vrtnu/a-z/het-journaal/2021/het-journaal-het-journaal-%s-%s" time date-fmt))
+          (mpv-default-options `(,(format "--ytdl-raw-options=username=%s,password=%s" user pass))))
+    (mpv-play news-url)))
+
+(provide 'vrtnu)
+;;; vrtnu.el ends here
